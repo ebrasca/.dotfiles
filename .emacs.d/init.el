786abfc8756e3272300ce5858737de42f2ea1c20
Bruno Cichon <ebrasca@librepanther.com>
2024-07-06 22:01:51 +0200

    Emacs: Remove duplicated line

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -78,8 +78,6 @@
 ;;; Set repos
 ;;;-----------------------------------------------------------------------------
 
-(require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
